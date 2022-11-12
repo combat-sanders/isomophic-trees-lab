c5adb6a0cf705c9b33974c99114a218f120d65cf
bazdu <bazdukovvalentin@gmail.com>
2022-11-12 14:29:03 +1000

    add tree generator

---
+++ algorytm/generator.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define SWAP(a, b) ({__typeof__(a) t = a; a = b; b = t;})
+using namespace std;
+
+int rand(int a, int b) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(a, b);
+    return dist(gen);
+}
+
+vector<vector<int>> generate_tree(int vertex_count) {
+    vector<vector<int>> matrix(vertex_count);
+    for (int i = 1; i < matrix.size(); i++) {
+        int vertex_to_pair = rand(0, i - 1);
+        matrix[i].push_back(vertex_to_pair);
+        matrix[vertex_to_pair].push_back(i);
+    }
+    return matrix;
+}
+
+vector<vector<int>> generate_isomorphic(vector<vector<int>>& tree) {
+
+}
