f5124146e6b4732e30fdefccbb3594b6c38532bc
bazdu <bazdukovvalentin@gmail.com>
2023-01-27 06:23:07 +1000

    feat: Добавить заглушку для генератора

--- gen/generator.cpp
+++ gen/generator.cpp
@@ -1,22 +1,30 @@
 #include <bits/stdc++.h>
 #include "isomorphic.h"
+#include "generator.h"
 #include <random>
 using namespace std;
 
 random_device rd;
 mt19937 gen(rd());
+
 int rand(int a, int b) {
     uniform_int_distribution<> dist(a, b);
     return dist(gen);
 }
 
-vector<vector<int>> generate_tree(int vertex_count) {
-    vector<vector<int>> matrix(vertex_count);
-    for (int i = 1; i < matrix.size(); i++) {
-        int vertex_to_pair = rand(0, i - 1);
-        add_link(matrix, i, vertex_to_pair);
+vector<int> generate_tree(int vertex_count) {
+    vector<int> tree(vertex_count, -1);
+    for (int i = 1; i < tree.size(); i++) {
+        int vertex = rand(0, i - 1);
+        tree[i] = vertex;
     }
-    return matrix;
+    return tree;
 }
+
+vector<node> generate_isomorphic(std::vector<int>& source_tree, int parent_id) {
+
+}
+
+
 
 
--- gen/generator.h
+++ gen/generator.h
@@ -6,3 +6,7 @@
 #define ISOMORPHIC_TREES_LAB_GENERATOR_H
 
 #endif //ISOMORPHIC_TREES_LAB_GENERATOR_H
+
+
+vector<int> generate_tree(int vertex_count);
+vector<node> generate_isomorphic(const std::vector<int>& source_tree);
